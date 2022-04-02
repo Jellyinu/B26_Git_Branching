025c103889b43f328bd30838db74bf007fa96fea
Kristina J Lemus <Krispyjas100@gmail.com>
2022-04-02 11:45:56 -0700

    Created test case TC002

---
+++ src/test_cases/TC002.java
@@ -0,0 +1,14 @@
+package test_cases;
+
+public class TC002 {
+    public static void main(String[] args) {
+        //Test Case 2
+
+
+        // Writing  smoke tests
+
+        // creating a report
+
+        // Screenshot in failure
+    }
+}
